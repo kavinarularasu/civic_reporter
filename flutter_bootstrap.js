110b453d07d5f2e14f6ea34d65aafa661f687625
kavinarularasu <kavinarularasu@users.noreply.github.com>
2026-07-27 09:58:04 +0000

    deploy: fb192d19b9fecde7e6a201aabc96c73c6490596e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1303240375" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "862991334" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
